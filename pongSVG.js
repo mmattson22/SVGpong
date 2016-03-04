51bd0f3953f23fd7fd22528304e1a833059e4ab7
Michael Mattson <mhmattson@gmail.com>
2016-03-04 13:17:29 -0500

    y u no bounce?

--- pongSVG.js
+++ pongSVG.js
@@ -2,18 +2,36 @@ var pic = document.getElementById("vimg");
 var b1 = document.getElementById("animate");
 var id;
 
+
+
 var drawB = function(){
 
 	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 	var r = 5;
-	c.setAttribute( "cx", 250 );
-	c.setAttribute( "cy", 250 );
+	var dx = 1;
+	var dy = 1;
+	var cx = 250;
+	var cy = 250;
+	c.setAttribute( "cx", cx );
+	c.setAttribute( "cy", cy );
 	c.setAttribute( "r", r );
 	c.setAttribute( "fill", "red" );
 	c.setAttribute( "stroke", "black" );
 
 	pic.appendChild( c );
 
+	var bounce = function(){
+		if (cx = 500)
+			dx = -1;
+		if (cx = 0)
+			dx = 1;
+		if (cy = 500)
+			dy = -1;
+		if (cy = 0)
+			dy = 1;
+		c.setAttribute("cx", (cx+dx));
+		c.setAttribute("cy", (cy+dy));
+	}
+
+	id = setInterval(bounce, 640);
 };
-drawB();
-//b1.addEventListener("click",drawB);
